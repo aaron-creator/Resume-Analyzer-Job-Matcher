3ca0055de038f915d6904a5265ced23fbfb5b57e
aaron_tetracube <aaronpsam@gmail.com>
2025-03-31 11:34:07 +0530

    updated app.py with match_score.

--- backend/app.py
+++ backend/app.py
@@ -19,3 +19,14 @@ def preprocess_text(text):
     return "".join(tokens)                      #Joins the tokens with Spaces.
 
 @app.route("/match", methods=["POST"])          # routing for resume upload URL.
+def match_resume():
+    data = request.json                                             # data received from API
+    resume_text = preprocess_text(data.get("resume"))               # preprocessing resume text
+    job_desc_text = preprocess_text(data.get("job_description"))    # preprocessing Job description text
+
+    # creates tfidVectorizer used to convert text into numerical values based on word importance.
+    # Words that appear frequently in a document but rarely in others are given higher importance.
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform([resume_text,job_desc_text])
+    similarity = cosine_similarity(vectors[0],vectors[1])[0][0]
+    return jsonify({"match_score": similarity})
